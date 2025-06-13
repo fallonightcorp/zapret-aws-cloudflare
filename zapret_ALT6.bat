b0e3ab608bc985ce33b976069db348344951e139
lxstwxrden <48512277+V3nilla@users.noreply.github.com>
2025-06-14 03:56:18 +0400

    zapret_ALT6

---
+++ zapret_ALT6.bat
@@ -0,0 +1,81 @@
+:: Сделано Dronatar для zapret-discord-youtube версии 1.7.2b
+:: За основу взяты general (ALT2) из https://github.com/Flowseal/zapret-discord-youtube/releases
+:: и discord (ALT2) из https://github.com/Flowseal/zapret-discord-youtube/discussions/252#discussioncomment-13422081
+
+@echo off
+chcp 65001 > nul
+:: Set UTF-8 encoding for cmd.exe
+title zapret_ALT6
+
+cd /d "%~dp0"
+
+:: Call other service scripts to check status and updates
+call service.bat status_zapret
+
+:: Set paths for binaries and lists
+set "BIN=%~dp0bin\"
+set "LISTS=%~dp0lists\"
+
+:: === AWS IP UPDATE ===
+set "listPath=lists\list-aws-amazon.txt"
+set ipCountBefore=0
+set ipCountAfter=0
+set ipDelta=0
+
+:: Check if the list file exists and is not empty
+if exist "%listPath%" (
+    for /f %%A in ('find /c /v "" ^< "%listPath%"') do set ipCountBefore=%%A
+)
+
+:: If the file is empty or doesn't exist, set current IP count to 0
+if not defined ipCountBefore set ipCountBefore=0
+echo [INFO] Updating Amazon AWS IP list... (current: %ipCountBefore%)
+
+:: Call Python script to update AWS IP list with timeout
+start /wait /min "" python "%~dp0update-aws.py"
+
+:: Check if the update was successful or timed out
+if %ERRORLEVEL% neq 0 (
+    echo [ERROR] Failed to update IP list. Timeout or another issue occurred.
+) else (
+    :: Count IPs after update
+    if exist "%listPath%" (
+        for /f %%A in ('find /c /v "" ^< "%listPath%"') do set ipCountAfter=%%A
+    )
+
+    :: If file is still empty after update, set after count to 0
+    if not defined ipCountAfter set ipCountAfter=0
+    set /a ipDelta=%ipCountAfter% - %ipCountBefore%
+
+    echo [INFO] Updated IPs: was %ipCountBefore%, now %ipCountAfter%, added %ipDelta%
+    echo.
+)
+
+:: === User Selection for Strategy ===
+:MENU
+echo Select a strategy:
+echo [1] Strategy 1 (Fixed TTL (Protective))
+echo [2] Strategy 2 (Auto TTL (Fastest))
+set /p choice=Enter your choice (1 or 2): 
+
+:: Check valid
+if "%choice%"=="1" (
+    echo [INFO] Using Strategy 1
+    set strategy=--filter-udp=444-65535 --ipset="%LISTS%list-aws-amazon.txt" --dpi-desync-ttl=8 --dpi-desync-repeats=20 --dpi-desync-fooling=none --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp="%BIN%quic_initial_www_google_com.bin" --dpi-desync=fake --dpi-desync-cutoff=n10
+) else if "%choice%"=="2" (
+    echo [INFO] Using Strategy 2
+    set strategy=--filter-udp=444-65535 --ipset="%LISTS%list-aws-amazon.txt" --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=10 --dpi-desync-any-protocol=1 --dpi-desync-fake-unknown-udp="%BIN%quic_initial_www.google_com.bin" --dpi-desync-cutoff=n2
+) else (
+    echo [ERROR] Invalid choice! Please select a valid option.
+    goto MENU
+)
+
+:: === Launching Application with Parameters ===
+start "zapret_ALT6 | Strategy %choice%" /min "%BIN%winws.exe" --wf-tcp=80,443,444-65535 --wf-udp=443,444-65535,50000-50099,0-65535 ^
+--comment Discord(Calls) --filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-fake-discord=0x00 --dpi-desync-fake-stun=0x00 --dpi-desync-repeats=6 --new^
+--comment Discord --filter-tcp=443 --hostlist="%LISTS%list-discord.txt" --hostlist-domains=cdn.discordapp.com,gateway.discord.gg,images-ext-1.discordapp.net,media.discordapp.net --dpi-desync=fake --dpi-desync-fake-tls-mod=none --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,hopbyhop2 --new^
+--comment YouTube(QUIC) --filter-udp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new^
+--comment YouTube(Streaming) --filter-tcp=80 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,split2 --dpi-desync-fooling=md5sig --new^
+--filter-tcp=443 --hostlist-exclude="%LISTS%list-discord.txt" --hostlist="%LISTS%list-general.txt" --hostlist-domains=adguard.com,adguard-vpn.com,totallyacdn.com,whiskergalaxy.com,windscribe.com,windscribe.net,cloudflareclient.com,soundcloud.com,sndcdn.com,soundcloud.cloud --dpi-desync=split2 --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=1 --dpi-desync-repeats=2 --dpi-desync-fooling=badseq,hopbyhop2 --dpi-desync-split-seqovl-pattern="%BIN%tls_clienthello_www_google_com.bin" --new^
+--comment Cloudflare WARP(1.1.1.1, 1.0.0.1) --filter-tcp=443 --ipset-ip=162.159.36.1 --filter-l7=tls --dpi-desync=fake --dpi-desync-fake-tls=0x00 --dpi-desync-start=n2 --dpi-desync-cutoff=n3 --dpi-desync-fooling=md5sig --new^
+--comment WireGuard --filter-udp=0-65535 --filter-l7=wireguard --dpi-desync=fake --dpi-desync-fake-wireguard=0x00 --dpi-desync-cutoff=n2
